1c27a328113657dd2b5a2b2f2c3628ea0ef28c13
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-04-10 22:02:55 +0530

    Controlled Components - Expenses Filter

---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ExpensesFilter = () => {
+  return (
+    <div>
+        <div>
+            <select>
+                <option value="2023" >2023</option>
+                <option value="2024" >2024</option>
+                <option value="2025" >2025</option>
+            </select>
+        </div>
+
+    </div>
+  )
+}
+
+export default ExpensesFilter
