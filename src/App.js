5ab2ae92820807af62c79361da47773867f02eb7
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-04-10 21:12:53 +0530

    Rendering Expenses

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 // Write your code at relevant places in the code below:
 
-import React, { useState} from "react";
+import React ,{ useState} from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
-  const [expenses,setExpenses] = useState([
-    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
-    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
-    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
-    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
+  const [expenses, setExpenses] = useState([
+    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), amount: 100 },
+    { id: 2, title: "Book", date: new Date(2023, 8, 25), amount: 10 },
+    { id: 3, title: "Pen", date: new Date(2023, 2, 10), amount: 1 },
+    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), amount: 200 },
   ]);
 
-  const saveExpenseDataHandler = (enteredExpenseData) => {
+  /*in the rpev question its name was const saveExpenseDataHandler = (expenseData) => {
+    console.log(expenseData);*/
 
-    const expenseData = { ...enteredExpenseData, id: Math.random().toString() };
-    
-  setExpenses((prevExpenses) => [...prevExpenses, expenseData]);
-  console.log(expenseData)
+  const addExpenseHandler = (expense) => { 
+    console.log(expense);
+
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, expense];
+
+    });
+
   };
 
+  
 
   return (
     <div>
-      <NewExpense onSaveExpenseData={saveExpenseDataHandler} />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
