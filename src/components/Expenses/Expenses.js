3ee1368acf8d7d664cce07eb226923bdad80f024
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-04-11 18:50:09 +0530

    Conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,23 +17,42 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length === 1) {
+    expensesContent = (
+      <div>
+        <ExpenseItem
+          key={filteredExpenses[0].id}
+          title={filteredExpenses[0].title}
+          date={filteredExpenses[0].date}
+          price={filteredExpenses[0].price}
+        />
+        <p>Only one expense found. Please add more</p>
+      </div>
+    );
+  }
+
+  if (filteredExpenses.length > 1) {
+    expensesContent = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          date={expense.date}
+          price={expense.price}
+        />
+      );
+    });
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={changeFilterHandler}
       />
-      {filteredExpenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            date={expense.date}
-            price={expense.price}
-          />
-        );
-      })}
+      {expensesContent}
     </Card>
   );
 };
